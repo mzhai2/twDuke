eba0ad550126e710dce1c79ef608ffb39d35a7df
mzhai2 <mzhai2@emory.edu>
2013-11-16 18:58:05 -0500

    ex Search

--- main.py
+++ main.py
@@ -10,8 +10,11 @@
 
 from twython import Twython
 
-APP_KEY = kK38G4kaJ96PjsTVeLydA
-APP_SECRET = yLFu9sgN7Bw0e3QWuXzHzOts9zkPaojmRRVDnNE8vhY
+APP_KEY = 'kK38G4kaJ96PjsTVeLydA'
+APP_SECRET = 'yLFu9sgN7Bw0e3QWuXzHzOts9zkPaojmRRVDnNE8vhY'
 
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
+twitter = Twython(APP_KEY, access_token = ACCESS_TOKEN)
+
+twitter.search(q='python', result_type='popular')
