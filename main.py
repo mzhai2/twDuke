c3e08feedd51d1c097d0eb9532e6216572775443
mzhai2 <mzhai2@emory.edu>
2013-11-16 22:09:32 -0500

    Revert "streamer ex"
    This reverts commit 541ff6ebb3e5714bf951fee451fff9f233c4c8fd.
    
    Conflicts:
    	main.py

--- main.py
+++ main.py
@@ -9,9 +9,8 @@
 #Access level 	Read-only
 
 from twython import Twython
-from twython import TwythonStreamer
-import sys, getopt
 
+<<<<<<< HEAD
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
@@ -52,3 +51,13 @@ OAUTH_TOKEN_SECERT = final_step[OAUTH_TOKEN_SECRET]
 
 #stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 #stream.statuses.filter(track='twitter')
+=======
+APP_KEY = 'kK38G4kaJ96PjsTVeLydA'
+APP_SECRET = 'yLFu9sgN7Bw0e3QWuXzHzOts9zkPaojmRRVDnNE8vhY'
+
+twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+ACCESS_TOKEN = twitter.obtain_access_token()
+twitter = Twython(APP_KEY, access_token = ACCESS_TOKEN)
+
+twitter.search(q='python', result_type='popular')
+>>>>>>> parent of 541ff6e... streamer ex
